8d6d065d7373d60e272d87fa727795cc1cbacbf3
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-22 11:11:41 +0300

    add functionality to the readmore button and go back button in card 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $("#hiddenp,.btn2").hide();
+    $(".btn1").click(function(){
+        $(".btn2,#hiddenp").show();
+    })
+    $(".btn2").click(function(){
+        $(".btn2,#hiddenp").hide();
+    })
     
-    alert("Lorraine")
+
 })
