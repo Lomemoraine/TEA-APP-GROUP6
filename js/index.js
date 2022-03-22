20d652446fd309373c9f10ffb11da92a81f4e045
estheradwets <estheradwets00@gmail.com>
2022-03-22 18:14:50 +0300

    Add js script to the submit message and a message received alert

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     
     alert("Lorraine")
+
+    $("#submit").click(function(e){
+        e.preventDefault()
+        var name = $("#jina").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        alert(`Hey ${name}, Karibu sana TEA-APP. Just to be sure your message is : ${message}`)
+      });
 })
