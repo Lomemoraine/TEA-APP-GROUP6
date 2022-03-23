6b5c3644a14d0bf9326463c351bea594276f586f
Q Adipo <quincedya@gmail.com>
2022-03-24 01:13:52 +0300

    Add functions to get login specifier and create the array identifier

--- js/profile.js
+++ js/profile.js
@@ -4,5 +4,25 @@ $(document).ready(function(){
         $("#weightinput").show();
     });
     
-    })
+    // Useful in creating unique array identifier
+    var identifierVar = '';
+
+    function getLoginValue (){
+        identifierVar = document.getElementById("#").value.toString();
+        return identifierVar;
+    });
+
+    function arraySpecifier (identifierVar){
+        var date = "date";
+        var weight = "weight";
+
+        date += identifierVar;
+        weight += identifierVar;
+
+        return date, weight;
+    }
+
+    
+
+
     
