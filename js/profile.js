11180eeed3da90ef6d204c68283699f012f07305
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-24 20:47:33 +0300

    add functionality to summary table

--- js/profile.js
+++ js/profile.js
@@ -6,14 +6,12 @@ $(document).ready(function(){
         $("#weightinput").show();
     });
 
-    $("#month-summarybtn").click(function(){
-        $("#tableSummary").toggle();  
-    })
+    
     $("#recordbtn").click(function(){
         $("#select-to-view").toggle(); 
     })
 
-    var userName = 'john';
+    var userName = 'jane';
     var sumArray
     // var userName = document.getElementById("#").value.toString();
 
@@ -36,21 +34,21 @@ $(document).ready(function(){
     if (userName == 'john'){
         var name = 'John Kirui'
         var email = 'john.kirui@kapkatetea.com'
-        var date = ['22/01/2022','27/01/2022','07/02/2022','10/02/2022','14/02/2022','21/02/2022'];
+        var day = ['22/01/2022','27/01/2022','07/02/2022','10/02/2022','14/02/2022','21/02/2022'];
         var weight = [30,45,61,32,23,41];
         var sumArray = sumAll(weight);     
 
     } else if (userName == 'jane'){
         var name = 'Jane Chepkemoi'
         var email = 'janechep@yahoo.com'
-        var date = ['27/01/2022','02/02/2022','09/02/2022','13/02/2022','21/02/2022','01/03/2022'];
+        var day = ['27/01/2022','02/02/2022','09/02/2022','13/02/2022','21/02/2022','01/03/2022'];
         var weight = [123,56,76,46,92,107];
         var sumArray = sumAll(weight);
 
     } else if (userName == 'roberts'){
         var name = 'Robert Langat'
         var email = 'lroberts@gmail.com'
-        var date = ['02/01/2022','13/01/2022','19/01/2022','26/01/2022','03/02/2022','16/02/2022','19/02/2022','26/02/2022','04/03/2022'];
+        var day = ['02/01/2022','13/01/2022','19/01/2022','26/01/2022','03/02/2022','16/02/2022','19/02/2022','26/02/2022','04/03/2022'];
         var weight = [111,56,57,113,45,65,135,32,47];
         var sumArray = sumAll(weight);
 
@@ -63,7 +61,7 @@ $(document).ready(function(){
     $("#email").text(email);
     $("#totalSupplied").text(sumArray);
 
-
+   
    
     
     //Adding an element to the array
@@ -74,7 +72,7 @@ $(document).ready(function(){
     //     newDate = document.getElementById("#day").value.toString();
     //     newWeight = parseInt(document.getElementById("#weight").value);
 
-    //     date = date.push(newDate);
+    //     day = day.push(newDate);
     //     weight = weight.push(newWeight);
 
     // };
@@ -91,6 +89,42 @@ $(document).ready(function(){
         $("#totalSupplied").html(sumArray);
 
     });
+    // function weigh(daysdate,newInput,sumArray){
+    //     this.daysdate =daysdate;
+    //     this.newInput=newInput;
+    //     this.sumArray=sumArray;
+    // }
+    // $("#month-summarybtn").click(function(e){
+    //     e.preventDefault();
+    //    $("#tableSummary").toggle(); 
+    //     let daysdate =$("day").val();
+    //     let newInput =$("weight").val();
+    //     sumArray = sumArray + parseInt(newInput);
+    //     console.log(daysdate);
+    //     console.log(newInput);
+    //     console.log(sumArray);
+    //     let weigh1 = new weigh(daysdate,newInput,sumArray)
+
+    // })
+    $("#month-summarybtn").click(function(e){
+         e.preventDefault();
+        $("#tableSummary").toggle(); 
+        let daysdate =$("#day").val();
+        let newInput =$("#weight").val();
+        sumArray = sumAll(weight);
+       let monthsTotal = parseInt(newInput) +parseInt(sumArray);
+
+       $("#tableSummary").append(
+        `<tr>
+         <td>${daysdate}<td>
+         <td>${newInput}<td>
+         <td>${monthsTotal}<td>
+         
+         <tr>
+        `   
+       )
+    })
+
 
 
     
