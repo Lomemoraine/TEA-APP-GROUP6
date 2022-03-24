670cfa4ecf87e3e378919c50409a98889d69d167
Q Adipo <quincedya@gmail.com>
2022-03-24 05:00:19 +0300

    Select the holding container

--- js/profile.js
+++ js/profile.js
@@ -1,32 +1,31 @@
 $(document).ready(function(){
     $("#weightinput").hide();
     $("#updateRecord").click(function(){
-        $("#weightinput").show();
+        $("#weightinput").show
 
+    var userName = '';
+    var ipputDate = '';
+    var inputTime = '';
+
+    // Data containers
+    if (userName == ''){
+        var date_1 = [''];
+        var weight_1 = [''];
+
+        
+    } else if (userName == ''){
+        var date_2 = [''];
+        var weight_2 = [''];
+    } else if (userName == ''){
+        var date_3 = [''];
+        var weight_3 = [''];
+    } else {
+        alert("No records found")
+    }
+
+
+    // Summation of the net weight function
     
-        // Useful in creating unique array identifier
-    var identifierVar = '';
-
-    function getLoginValue (){
-        identifierVar = document.getElementById("#").value.toString();
-        return identifierVar;
+    
     });
-
-    function arraySpecifier (identifierVar){
-        var date = "date";
-        var weight = "weight";
-
-        date += identifierVar;
-        weight += identifierVar;
-
-        return date, weight;
-    };
-
-    });
-    
-    
-
-
-
-
     
