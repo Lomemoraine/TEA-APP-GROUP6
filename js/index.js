94513f8de9842abccef3e361ede9cb7b7f80a8f9
faith62 <phifeichemu@gmail.com>
2022-03-24 10:34:28 +0300

    add show and hide functionality on the pages

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
     
     alert("Lorraine")
+    $("#sign-sign-in span").click(function(){
+        $("#sign-up").show()
+        $("#sign-in").hide()
+
+    })
+    $("#form-sign-up span").click(function(){
+        $("#sign-in").show()
+        $("#sign-up").hide()
+
+    })
+    
+    
 })
