33e017999b20b97ab7924f8d259325969ea5a824
Q Adipo <quincedya@gmail.com>
2022-03-24 05:12:51 +0300

    Add sum all function and the update records function

--- js/profile.js
+++ js/profile.js
@@ -25,6 +25,28 @@ $(document).ready(function(){
 
 
     // Summation of the net weight function
+    function sumAll(){
+        var i;
+        var sum = 0;
+        for (i=0; i < arguments.length; i++){
+            sum += arguments[i];
+        }
+        return sum
+    }
+
+    //Adding an element to the array
+    function updateRecords(){
+        var newDate = '';
+        var newWeight = '';
+
+        newDate = document.getElementById("#").value.toString();
+        newWeight = parseInt(document.getElementById("#").value);
+
+        date.push(newDate);
+        weight.push(newWeight);
+
+    }
+
     
     
     });
